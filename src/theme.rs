eae20c25801c1b5f8bcc75d980d56307aeef91e1
desbma <desbma@users.noreply.github.com>
2021-04-17 02:12:32 +0200

    Add missing file

---
+++ src/theme.rs
@@ -0,0 +1,10 @@
+pub enum Color {
+    Foreground = 0x93a1a1,
+    MainIcon = 0xeee8d5,
+    Focused = 0x2aa198,
+    Unfocused = 0x657b83,
+    Good = 0x859900,
+    Notice = 0xb58900,
+    Attention = 0xcb4b16,
+    Critical = 0xdc322f,
+}
