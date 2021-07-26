d089c7bcae9eeea6fdcd659c3a81841ad077ec99
desbma <desbma@users.noreply.github.com>
2021-07-26 17:43:41 +0200

    Bluetooth: Rendering complete

--- src/polybar_module/bluetooth.rs
+++ src/polybar_module/bluetooth.rs
@@ -8,7 +8,16 @@ use crate::theme;
 pub struct BluetoothModule {}
 
 #[derive(Debug, PartialEq)]
-pub struct BluetoothModuleState {}
+struct BluetoothDevice {
+    connected: bool,
+    name: String,
+}
+
+#[derive(Debug, PartialEq)]
+pub struct BluetoothModuleState {
+    controller_powered: bool,
+    devices: Vec<BluetoothDevice>,
+}
 
 impl BluetoothModule {
     pub fn new() -> anyhow::Result<BluetoothModule> {
@@ -16,7 +25,10 @@ impl BluetoothModule {
     }
 
     fn try_update(&mut self) -> anyhow::Result<BluetoothModuleState> {
-        Ok(BluetoothModuleState {})
+        Ok(BluetoothModuleState {
+            controller_powered: false,
+            devices: vec![],
+        })
     }
 }
 
@@ -41,11 +53,34 @@ impl RenderablePolybarModule for BluetoothModule {
 
     fn render(&self, state: &Self::State) -> String {
         match state {
-            Some(_state) => {
-                format!(
-                    "{} ",
-                    markup::style("", Some(theme::Color::MainIcon), None, None, None),
-                )
+            Some(state) => {
+                let mut fragments: Vec<String> = vec![format!(
+                    "{} {}",
+                    markup::style("", Some(theme::Color::MainIcon), None, None, None),
+                    if state.controller_powered {
+                        ""
+                    } else {
+                        ""
+                    },
+                )];
+                for device in &state.devices {
+                    fragments.push(markup::style(
+                        &format!(
+                            "{}{}",
+                            if device.connected { "" } else { "" },
+                            device.name
+                        ),
+                        None,
+                        if device.connected {
+                            Some(theme::Color::Foreground)
+                        } else {
+                            None
+                        },
+                        None,
+                        None,
+                    ));
+                }
+                fragments.join(" ")
             }
             None => markup::style("", Some(theme::Color::Attention), None, None, None),
         }
@@ -60,8 +95,35 @@ mod tests {
     fn test_render() {
         let module = BluetoothModule::new().unwrap();
 
-        let state = Some(BluetoothModuleState {});
-        assert_eq!(module.render(&state), "%{F#eee8d5}%{F-} ");
+        let state = Some(BluetoothModuleState {
+            controller_powered: false,
+            devices: vec![],
+        });
+        assert_eq!(module.render(&state), "%{F#eee8d5}%{F-} ");
+
+        let state = Some(BluetoothModuleState {
+            controller_powered: true,
+            devices: vec![],
+        });
+        assert_eq!(module.render(&state), "%{F#eee8d5}%{F-} ");
+
+        let state = Some(BluetoothModuleState {
+            controller_powered: true,
+            devices: vec![
+                BluetoothDevice {
+                    connected: false,
+                    name: "D1".to_string(),
+                },
+                BluetoothDevice {
+                    connected: true,
+                    name: "D2".to_string(),
+                },
+            ],
+        });
+        assert_eq!(
+            module.render(&state),
+            "%{F#eee8d5}%{F-}  D1 %{u#93a1a1}%{+u}D2%{-u}"
+        );
 
         let state = None;
         assert_eq!(module.render(&state), "%{F#cb4b16}%{F-}");
